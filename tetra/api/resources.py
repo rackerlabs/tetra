63974c309366145d7fcdee22393d6dac2318c2b4
Melissa Kam <melissa.kam@rackspace.com>
2016-05-12 17:37:20 -0500

    Removing suites and using builds instead
    - Combined suites and builds into a resource that more corresponds to an execution of a Jenkins job
    - Moved defining information like region and environment out of results and into builds

--- tetra/api/resources.py
+++ tetra/api/resources.py
@@ -21,7 +21,6 @@ import xunitparser
 from tetra.data.models.build import Build
 from tetra.data.models.project import Project
 from tetra.data.models.result import Result
-from tetra.data.models.suite import Suite
 
 
 def make_error_body(msg):
@@ -75,81 +74,25 @@ class ProjectsResource(Resources):
     RESOURCE_CLASS = Project
 
 
-class SuitesResource(Resources):
-    ROUTE = "/{project_id}/suites/"
-    RESOURCE_CLASS = Suite
-
-
-class SuiteResource(Resource):
-    ROUTE = "/{project_id}/suites/{suite_id}"
-    RESOURCE_CLASS = Suite
-    RESOURCE_ID_KEY = "suite_id"
-
-
 class BuildsResource(Resources):
-    ROUTE = "/{project_id}/suites/{suite_id}/builds"
+    ROUTE = "/{project_id}/builds"
     RESOURCE_CLASS = Build
-
-    def on_get(self, req, resp, **kwargs):
-        resp.status = falcon.HTTP_200
-        kwargs.update(req.params)
-        builds = self.RESOURCE_CLASS.get_all(**kwargs)
-
-        for build in builds:
-            results = Result.get_all(project_id=build['project_id'],
-                                     suite_id=build['suite_id'],
-                                     build_num=build['build_num'])
-            build['results'] = results.get("metadata")
-
-        resp.body = json.dumps(builds)
-
-    def on_post(self, req, resp, **kwargs):
-        resp.status = falcon.HTTP_201
-        data = req.stream.read()
-        data_dict = json.loads(data)
-        project_id = kwargs.get("project_id")
-        suite_id = kwargs.get("suite_id")
-        data_dict['project_id'] = project_id
-        data_dict['suite_id'] = suite_id
-        build = Build.from_dict(data_dict)
-        created_result = Build.create(resource=build)
-        resp.body = json.dumps(created_result.to_dict())
 
 
 class BuildResource(Resource):
-    ROUTE = "/{project_id}/suites/{suite_id}/builds/{build_id}"
+    ROUTE = "/{project_id}/builds/{build_id}"
     RESOURCE_CLASS = Build
     RESOURCE_ID_KEY = "build_id"
 
 
-class BuildResultsResource(Resources):
-    ROUTE = "/{project_id}/suites/{suite_id}/builds/{build_id}/results"
-    RESOURCE_CLASS = Result
-
-    # a result is tied to a build via the build_num only, so we can ignore
-    # the build_id when listing or posting results for this route.
-    def on_get(self, req, resp, build_id, **kwargs):
-        return super(BuildResultsResource, self).on_get(req, resp, **kwargs)
-
-    def on_post(self, req, resp, build_id, **kwargs):
-        return super(BuildResultsResource, self).on_post(req, resp, **kwargs)
-
-
-class BuildResultResource(Resource):
-    ROUTE = ("/{project_id}/suites/{suite_id}/builds/{build_id}"
-             "/results/{result_id}")
-    RESOURCE_CLASS = Result
-    RESOURCE_ID_KEY = "result_id"
-
-
-class SuiteResultsResource(Resources):
-    ROUTE = "/{project_id}/suites/{suite_id}/results"
+class ResultsResource(Resources):
+    ROUTE = "/{project_id}/builds/{build_id}/results"
     RESOURCE_CLASS = Result
 
     def on_post(self, req, resp, **kwargs):
         if self._is_junit_xml_request(req):
             return self._on_post_junitxml(req, resp, **kwargs)
-        return super(SuiteResultsResource, self).on_post(req, resp, **kwargs)
+        return super(ResultsResource, self).on_post(req, resp, **kwargs)
 
     def _is_junit_xml_request(self, req):
         return req.content_type and 'application/xml' in req.content_type
@@ -157,16 +100,16 @@ class SuiteResultsResource(Resources):
     def _on_post_junitxml(self, req, resp, **kwargs):
         resp.status = falcon.HTTP_201
 
-        suite, _ = xunitparser.parse(req.stream)
+        build, _ = xunitparser.parse(req.stream)
         results = [
             Result.from_junit_xml_test_case(case, req, **kwargs)
-            for case in suite
+            for case in build
         ]
         response_data = Result.create_many(results, **kwargs)
         resp.body = json.dumps(response_data)
 
 
-class SuiteResultResource(Resource):
-    ROUTE = "/{project_id}/suites/{suite_id}/results/{result_id}"
+class ResultResource(Resource):
+    ROUTE = "/{project_id}/builds/{build_id}/results/{result_id}"
     RESOURCE_CLASS = Result
     RESOURCE_ID_KEY = "result_id"
--- tetra/data/models/build.py
+++ tetra/data/models/build.py
@@ -13,8 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import time
-
 from tetra.data import sql
 from tetra.data.models.base import BaseModel
 
@@ -23,11 +21,12 @@ class Build(BaseModel):
 
     TABLE = sql.builds_table
 
-    def __init__(self, project_id, suite_id, build_num,
-                 id=None, timestamp=None):
+    def __init__(self, project_id, name, id=None, build_url=None, region=None,
+                 environment=None):
         if id:
             self.id = int(id)
         self.project_id = int(project_id)
-        self.suite_id = int(suite_id)
-        self.build_num = int(build_num)
-        self.timestamp = timestamp or time.time()
+        self.name = name
+        self.build_url = build_url
+        self.region = region
+        self.environment = environment
